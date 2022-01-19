5195e791467194bc0611ac45b61c2467cf42255d
Leonardo Freitas <leofreitas.engineer@gmail.com>
2022-01-18 21:30:56 -0300

    JavaScript course

---
+++ Frontend/JavaScript/complete-javascript-course-master/01-Fundamentals-Part-1/starter/challenge1.js
@@ -0,0 +1,25 @@
+let mass, height, markBmi, johnBmi, markHigherBMI
+
+mass = 92
+height = 1.95
+johnBmi = mass / (height * height)
+
+mass = 78
+height = 1.69
+markBmi = mass / (height * height)
+
+markHigherBMI = markBmi > johnBmi
+
+console.log(markHigherBMI)
+
+mass = 85
+height = 1.76
+johnBmi = mass / (height * height)
+
+mass = 95
+height = 1.88
+markBmi = mass / (height * height)
+
+markHigherBMI = markBmi > johnBmi
+
+console.log(markHigherBMI)
---
+++ Frontend/JavaScript/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,4 @@
+let js = 'amazing'
+console.log('Jonas')
+
+console.log(typeof js)
